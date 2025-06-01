bd394f04c758731d217a708a9c2a53ecb7e3c5a1
typograaf <148978827+typograaf@users.noreply.github.com>
2025-06-01 21:04:30 +0200

    Add files via upload

---
+++ netlify/functions/test.js
@@ -0,0 +1,13 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      message: 'Hello from test function!',
+      timestamp: new Date().toISOString()
+    })
+  };
+};
